8762512e9d068359af368ae8f83be5f4391fa57b
Swatitomar2559 <swatitomar051@gmail.com>
2024-09-29 20:07:45 +0530

    Add files via upload

---
+++ day 2 SUBSCRIBE_.cpp
@@ -0,0 +1,20 @@
+
+
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    double n,x;
+	    cin>>n>>x;
+	    int r=ceil(n/6);
+	    cout<<r*x<<endl;
+	}
+	return 0;
+}
