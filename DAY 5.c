0bf820f203ee6e2ddd016910a874a942178aef8c
Swatitomar2559 <swatitomar051@gmail.com>
2024-10-02 21:33:18 +0530

    Add files via upload

---
+++ DAY 5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+	int T;
+    scanf("%d", &T);
+    
+    while(T--) {
+        int B1, B2, B3;
+        scanf("%d %d %d", &B1, &B2, &B3);
+        
+        // Check if at least two bottles are empty
+        if (B1 + B2 + B3 <= 1) {
+            printf("Water filling time\n");
+        } else {
+            printf("Not now\n");
+        }
+    }
+    
+    return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+}
+
