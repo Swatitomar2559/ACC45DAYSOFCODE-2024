ac24292cb256250263963439b9abbcebd4aea5cd
Swatitomar2559 <swatitomar051@gmail.com>
2024-10-09 22:02:27 +0530

    Add files via upload

---
+++ day 12.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+int t;
+cin>>t;
+while(t--){
+    
+    int n,x;
+    cin>>n>>x;
+    cout<<min(x,n-x)<<endl;
+    
+    
+}
+	
+	
+	return 0;
+	
+	
+	
+	
+}
