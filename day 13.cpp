76c1be094625cca60b7d22783945954824cf4741
Swatitomar2559 <swatitomar051@gmail.com>
2024-10-10 21:57:34 +0530

    Add files via upload

---
+++ day 13.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	 int t;
+	 cin>>t;
+	 while(t--)
+	 {
+	     int x,y;
+	     cin >> x >>y;
+	     if(y >=(x+1)/2){
+	         cout<<"yes"<<endl;
+	     }
+	     else
+	     
+	     cout<<"no"<<endl;
+	 }
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+
+}
